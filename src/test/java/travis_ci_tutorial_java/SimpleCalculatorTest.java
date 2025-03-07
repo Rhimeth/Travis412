15a3b0dc13b71561c08993af9613b3a202267ac3
dubemntiwunka <61565714+Rhimeth@users.noreply.github.com>
2025-03-07 16:32:38 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -1,14 +1,19 @@
 package travis_ci_tutorial_java;
 
-import java.util.*;
-
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SimpleCalculatorTest {
-	@Test
-	public void testAdd() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 2);
-	}
+
+    @Test
+    public void testAdd() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(2, calc.add(1, 1));
+    }
+
+    @Test
+    public void testMinus() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(2, calc.minus(5, 3));
+    }
 }
