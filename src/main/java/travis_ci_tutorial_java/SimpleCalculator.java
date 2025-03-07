1d7a02352a7c50e73f6a9ea681b4313a5b9baac4
dubemntiwunka <61565714+Rhimeth@users.noreply.github.com>
2025-03-07 17:17:42 -0500

    Update SimpleCalculator.java

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -9,4 +9,15 @@ public class SimpleCalculator {
     public int minus(int a, int b) {
         return a - b;
     }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide zero");
+        }
+        return (double) a / b;
+    }
 }
