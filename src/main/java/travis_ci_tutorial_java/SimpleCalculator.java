2d2d34214da19b6cc494b0b271a9e68b82e03eaa
dubemntiwunka <61565714+Rhimeth@users.noreply.github.com>
2025-03-07 17:05:24 -0500

    Update SimpleCalculator.java

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,7 +1,12 @@
 package travis_ci_tutorial_java;
 
 public class SimpleCalculator {
-	public int add(int a, int b) {
-		return a + b;
-	}
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
 }
